d644fe07acf49236a3eebe405fbd1633da0e1548
Luigi T <luigitejada@yahoo.com>
2022-10-27 22:21:39 -0400

    add filmingo to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
     <>
       <main className={styles.container}>
-        <h2>Welcome to Fantasy Filmmaking</h2>
+        <h2>Welcome to Filmingo Fantasy Filmmaking</h2>
         <h5>Click on <a href="http://localhost:3000/movie-search">Movie Search</a> to start making movie magic!</h5>
           <div className={styles.titleDiv}>
             <img src="https://i.postimg.cc/Tw0qXjpJ/ALG-Movie-Magic-Drop-Shadow-1.png" alt="flamingo" />
